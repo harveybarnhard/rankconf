248d77de39d1baea020f6669381ae9618b825bd9
Harvey Barnhard <harveybarnhard@gmail.com>
2021-10-22 14:04:01 -0400

    Visual updates to rank plots with uniform confidence sets

--- R/rankplot.R
+++ R/rankplot.R
@@ -5,18 +5,18 @@ rankplot = function(y, L, U, size=1, title="", highlight=c(0,0)) {
   color = rep("Neither", length(y))
   color[U < lowtail] = "good"
   color[L > uptail]  = "bad"
-  g = ggplot(data=data.frame(ranky, L, U, color)) +
-    geom_linerange(mapping=aes(x=ranky, ymin=L, ymax=U, colour=color),
+  g = ggplot2::ggplot(data=data.frame(ranky, L, U, color)) +
+    ggplot2::geom_linerange(mapping=aes(x=ranky, ymin=L, ymax=U, colour=color),
                    size=size) +
-    xlab("") + ylab("") +
-    scale_x_continuous(expand=c(0.01,0.01)) +
-    scale_y_continuous(expand=c(0.01,0.01)) +
-    scale_color_manual(values=c("#1a4c8b", "#E12900", "grey"), guide = FALSE) +
-    theme_classic() +
-    ggtitle(label=title) +
-    theme(
-      text = element_text(family="LM Roman"),
-      plot.title = element_text(hjust = 0.5)
+    ggplot2::xlab("") + ggplot2::ylab("") +
+    ggplot2::scale_x_continuous(expand=c(0.01,0.01)) +
+    ggplot2::scale_y_continuous(expand=c(0.01,0.01)) +
+    ggplot2::scale_color_manual(values=c("#1a4c8b", "#E12900", "grey"), guide = FALSE) +
+    ggplot2::theme_classic() +
+    ggplot2::ggtitle(label=title) +
+    ggplot2::theme(
+      text = ggplot2::element_text(family="LM Roman"),
+      plot.title = ggplot2::element_text(hjust = 0.5)
     )
   return(g)
 }
